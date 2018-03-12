de0c442138c2bbb77764dcde13971dc6c0abb778
Sandré Cardoso <habitsul@gmail.com>
2018-03-12 01:04:07 -0300

    Initial commit

---
+++ app/src/main/java/ideandroid/sandrecardoso/acer/ideandroid/MainActivity.java
@@ -0,0 +1,56 @@
+package ideandroid.sandrecardoso.acer.ideandroid;
+
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+import android.support.v7.widget.ShareActionProvider;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.compat.*;
+import android.widget.VideoView;
+import java.util.Objects;
+import android.widget.DatePicker;
+
+import ideandroid.sandrecardoso.acer.ideandroid.R.id;
+import ideandroid.sandrecardoso.acer.ideandroid.R.layout;
+import ideandroid.sandrecardoso.acer.ideandroid.R.mipmap;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText texto;
+    private Button ok;
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(layout.activity_main);
+        this.texto = this.findViewById(id.texto);
+        this.ok = this.findViewById(id.ok);
+        this.ok.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                if (MainActivity.this.texto.getText().toString().equals("alert")){
+                Builder alert=new Builder(MainActivity.this);
+                alert.setTitle("Aviso");
+                alert.setMessage("você deseja fechar!!!");
+                alert.setIcon(mipmap.ic_launcher_foreground);
+
+                alert.show();
+                }
+
+            }
+        });
+    }
+}
